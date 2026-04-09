8ee92057c0a4af5f721c224f571522f597ac83f0
Dipali Giri <dipaligiri11dpsm@gmail.com>
2026-04-09 12:45:41 +0530

    fix cors for production

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ const app = express();
 
 app.use(cors({
   origin: [
-    "https://cinematchrecs.netlify.app/",
+    "https://cinematchrecs.netlify.app",
     "http://localhost:5500",
     "http://127.0.0.1:5500"
   ]
 }));
-app.use(express.json());
+app.use(express.json()); 
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
